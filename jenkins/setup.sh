ca0760db8da3bcdf3720f53c44053ad8740e185c
Kyronne <57007690+Kyronne@users.noreply.github.com>
2021-11-15 21:29:23 +0000

    Update setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-# install apt dependencies
-sudo apt update
-sudo apt install python3-venv python3-pip python3 -y
+r# newdir
+mkdir newdir
+cd new dir 
+touch newfile 
 
-# create virtual environment
-python3 -m venv venv
 
-# activate virtual environment
-source venv/bin/activate
-
-# install our pip requirements (from requirements.txt)
-pip3 install -r requirements.txt
